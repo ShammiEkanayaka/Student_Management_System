97c8793b248fce92578687618bf26d5a40257179
Shyamali Ekanayaka <52402034+ShammiEkanayaka@users.noreply.github.com>
2020-01-16 18:00:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 require("dotenv").config();
-const express = require('express');
-const app = express();
-const userRouter = require("./api/users/user.router");
+var express = require('express');
+var app = express();
+var userRouter = require("./api/users/user.router");
 
-app.use(express.json());  // convert js objects into json  
+app.use(express.json());  // *convert the js objects into json objects.* 
 
 app.use("/api/users", userRouter);
-const bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt');
+//var PORT = process.env.PORT || 3000;   // *Not necessary to run PORT.* 
 
+app.listen(process.env.APP_PORT, () => {
+  console.log("Server up and running on PORT : ", process.env.APP_PORT);
 
-//const PORT = process.env.PORT || 3000;
-
-
-
-/*
+  /*
 app.get('/api', (req, res) => {
     res.json({
         success: 1,
@@ -21,13 +20,24 @@ app.get('/api', (req, res) => {
     });
   });
 */
-/*
-  app.listen(process.env.APP_PORT, () => {
-      console.log("Server up and running on port : ", process.env.APP_PORT);
-  });
-*/
-
-  app.listen(process.env.APP_PORT, () => {
-    console.log("Server up and running on PORT : ", process.env.APP_PORT);
-
 });
+
+
+
+/* FROM
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const userRouter = require("./api/users/user.router");
+
+app.use(express.json());
+
+app.use("/api/users", userRouter);
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log("server up and running on PORT :", port);
+});
+
+
+
+*/
