189f655baf1532f2aecfcd46ff97a71d928023c5
Shyamali Ekanayaka <52402034+ShammiEkanayaka@users.noreply.github.com>
2020-01-16 18:00:12 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,5 @@
-const { createPool } = require("mysql");
-
-
-
-const pool  = createPool({
+var { createPool } = require("mysql");
+var pool  = createPool({
     port:process.env.DB_PORT,  
     host:process.env.DB_HOST,  
     user:process.env.DB_USER, 
@@ -12,10 +9,9 @@ const pool  = createPool({
     });  
 
     module.exports = pool;
-    
-/*
 
-const pool  = createPool({
+/* without using .env file
+var pool  = createPool({
     port:3306,  
     host:"localhost",  
     user:"root", 
@@ -26,3 +22,23 @@ const pool  = createPool({
 
     module.exports = pool;
     */
+
+
+
+
+
+
+
+    /* FROM
+    const { createPool } = require("mysql");
+const pool = createPool({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  connectionLimit: 10
+});
+
+module.exports = pool;
+*/
